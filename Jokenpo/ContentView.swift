9abc4d30e43b186f9d2c707874feef1df12db68b
Gugas <souza.gs047@gmail.com>
2025-11-23 11:23:10 -0300

    Initial Commit

---
+++ Jokenpo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jokenpo
+//
+//  Created by Gustavo Souza Santana on 23/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jokenpo/JokenpoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokenpoApp.swift
+//  Jokenpo
+//
+//  Created by Gustavo Souza Santana on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct JokenpoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
